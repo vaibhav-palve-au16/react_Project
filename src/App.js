a54daedd85e91db92af55bc498420a10fbc15af8
vaibhav <vaibhavpalve1234@gmail.com>
2021-06-02 10:15:09 +0530

    done
    Signed-off-by: vaibhav <vaibhavpalve1234@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import React from "react";
+import { BrowseRouter as Router } from "react-router-bootstrap";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-  <>
-  <Header />
-    <main className='py-3'>
-    <Container>
-      <HomeScreen />
-    </Container>
-    </main>
-  <Footer />
-  </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
